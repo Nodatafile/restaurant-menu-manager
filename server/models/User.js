b05cb4589eae0031c46c52df38aa1fb79b2cf26d
Waxy1314 <148415250+Nodatafile@users.noreply.github.com>
2026-05-04 23:38:06 +0900

    Create User.js

---
+++ server/models/User.js
@@ -0,0 +1,83 @@
+// server/models/User.js
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  employeeId: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  department: {
+    type: String,
+    enum: ['reservation', 'operation', 'admin'],
+    required: true
+  },
+  role: {
+    type: String,
+    enum: ['manager', 'staff', 'supervisor'],
+    default: 'staff'
+  },
+  permissions: {
+    canManageReservations: { type: Boolean, default: false },
+    canManageStock: { type: Boolean, default: false },
+    canPlaceOrders: { type: Boolean, default: false },
+    canViewReports: { type: Boolean, default: false }
+  },
+  isActive: {
+    type: Boolean,
+    default: true
+  },
+  lastLogin: Date,
+  deviceTokens: [String] // 푸시 알림용
+}, {
+  timestamps: true
+});
+
+// 비밀번호 해싱
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+// 권한 자동 설정
+userSchema.pre('save', function(next) {
+  switch(this.department) {
+    case 'reservation':
+      this.permissions = {
+        canManageReservations: true,
+        canManageStock: false,
+        canPlaceOrders: true,
+        canViewReports: true
+      };
+      break;
+    case 'operation':
+      this.permissions = {
+        canManageReservations: false,
+        canManageStock: true,
+        canPlaceOrders: true,
+        canViewReports: true
+      };
+      break;
+    case 'admin':
+      this.permissions = {
+        canManageReservations: true,
+        canManageStock: true,
+        canPlaceOrders: true,
+        canViewReports: true
+      };
+      break;
+  }
+  next();
+});
+
+module.exports = mongoose.model('User', userSchema);
